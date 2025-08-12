68024922b8adc237646537e2ffc7a4a09d12fdf6
Luka Markušić <markusicluka@gmail.com>
2025-08-12 16:30:49 +0200

    Refactor zig solutions

--- 2015/zig/build.zig
+++ 2015/zig/build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -8,33 +8,27 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("utils.zig"),
     });
 
-    const exe01 = b.addExecutable(.{
-        .name = "01",
-        .root_source_file = b.path("01/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+    var sources = std.ArrayList([]const u8).init(b.allocator);
+    {
+        var dir = try std.fs.cwd().openDir("src", .{ .iterate = true });
+        defer dir.close();
 
-    exe01.root_module.addImport("utils", utils);
-    b.installArtifact(exe01);
+        var walker = dir.iterate();
 
-    const exe02 = b.addExecutable(.{
-        .name = "02",
-        .root_source_file = b.path("02/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
+        while (try walker.next()) |entry| {
+            try sources.append(entry.name);
+        }
+    }
 
-    exe02.root_module.addImport("utils", utils);
-    b.installArtifact(exe02);
+    for (sources.items) |source| {
+        const exe = b.addExecutable(.{
+            .name = source,
+            .root_source_file = b.path(b.pathJoin(&.{ "src", source })),
+            .target = target,
+            .optimize = optimize,
+        });
 
-    const exe03 = b.addExecutable(.{
-        .name = "03",
-        .root_source_file = b.path("03/main.zig"),
-        .target = target,
-        .optimize = optimize,
-    });
-
-    exe03.root_module.addImport("utils", utils);
-    b.installArtifact(exe03);
+        exe.root_module.addImport("utils", utils);
+        b.installArtifact(exe);
+    }
 }
